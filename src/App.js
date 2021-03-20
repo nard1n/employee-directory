5c0b83ba759cc29258be040779973537a0797883
Nardin Lachowski <nardin@live.com>
2021-03-20 14:56:17 -0400

    added componentts and util folders and file structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
-// import components dont forget
+import React, {Component} from 'react';
+//import components
+import NavBar from'./components/NavBar/NavBar';
+import Search from'./components/SearchBar/Search';
+import EmployeeList from'./components/EmployeeList/EmployeeList';
+import EmployeeCard from'./components/EmployeeCard/EmployeeCard';
+//import materialize
+import M from 'materialize-css';
+//import apis
+import API from './utils/API';
 
-function App() {
-  return (
-    <>
-    </>
-  );
+class App extends Component{
+  state = {
+
+  }
+  componentDidMount() {
+    M.AutoInit();
+  }
+  render() {
+    return (
+      <>
+        <NavBar />
+        <Search />
+      </>
+    );
+  }
 }
 
 export default App;
