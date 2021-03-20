57ce7ae307aa69ab935797cc6417ef0d8202f6fc
Nardin Lachowski <nardin@live.com>
2021-03-20 15:45:04 -0400

    implemented card into card component

--- src/components/EmployeeCard/EmployeeCard.js
+++ src/components/EmployeeCard/EmployeeCard.js
@@ -1,7 +1,29 @@
 import React from 'react';
 
 function EmployeeCard() {
-    return ("");
+    return (
+        <>
+        <div className="card">
+            <div className="card-image waves-effect waves-block waves-light">
+                <img className="activator" src="images/office.jpg" />
+            </div>
+            <div className="card-content">
+                <span className="card-title activator grey-text text-darken-4">
+                    Employee Name <i className="material-icons right">more_vert</i>
+                </span>
+                <p>
+                    <a href="#">This is a link</a>
+                </p>
+            </div>
+            <div className="card-reveal">
+                <span className="card-title grey-text text-darken-4">
+                    Employee Name<i className="material-icons right">close</i>
+                </span>
+                <p>Here is some more information about this employee.</p>
+            </div>
+        </div>
+        </>
+    );
 }
 
 export default EmployeeCard;
--- src/components/EmployeeList/EmployeeList.js
+++ src/components/EmployeeList/EmployeeList.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function EmployeeList() {
-    return ("");
+    return (
+        <>
+        
+        </>
+    );
 }
 
 export default EmployeeList;
