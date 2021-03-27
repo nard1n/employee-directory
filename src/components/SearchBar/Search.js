95db1eba63b7b0749a71bd8ef2cd65f14dc90f0a
Nardin Lachowski <nardin@live.com>
2021-03-27 19:11:52 -0400

    moved the Sort button out of file to be added into Sort.js component

--- src/components/SearchBar/Search.js
+++ src/components/SearchBar/Search.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Sort from '../Sort/Sort';
 
 function Search(props) {
   return (
@@ -14,14 +15,10 @@ function Search(props) {
                 onChange={props.change}
               />
               <label className="label-icon" for="search"></label>
-              <i className="material-icons">close</i>
             </div>
           </div>
-          <div className="col s3">
-            <button className="waves-effect waves-light btn-small">
-              Sort by Experience
-            </button>
-          </div>
+
+              <Sort sort={ props.sort } />
         </div>
       </form>
     </div>
