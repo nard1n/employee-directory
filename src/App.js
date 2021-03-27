bd2185af069dcb88157fd82aaae864dfaeb50706
Nardin Lachowski <nardin@live.com>
2021-03-26 23:50:09 -0400

    corrected the rendered components and added logic for Api search and content display based on search input

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 //import components
 import NavBar from'./components/NavBar/NavBar';
 import Search from'./components/SearchBar/Search';
@@ -11,18 +11,49 @@ import API from './utils/API';
 
 class App extends Component{
   state = {
-
+    search: "",
+    searchData: [],
+    directory: []
   }
+
   componentDidMount() {
     M.AutoInit();
+
+    API.search()
+      .then(res => this.setState({
+        directory: res.data.results,
+        searchData: res.data.results
+      }))
+      .catch(err => console.log(err));
   }
+
+  handleInputChange = event => {
+    const value = event.target.value;
+    let query = value.toLowerCase().trim();
+    const searchData = this.state.searchData;
+    let results = [];
+
+    for (let i = 0; i < searchData.length; i++) {
+      let person = searchData[i];
+      let fullName = (person.name.first + " " + person.name.last).toLowerCase();
+      console.log(fullName);
+      if (query === fullName.substring(0, query.length)) {
+        results.push(person);
+      }
+    }
+
+    this.setState({
+      search: value,
+      directory: results
+    });
+  }
+
   render() {
     return (
       <>
         <NavBar />
-        <Search />
-        <EmployeeList />
-        <EmployeeCard />
+        <Search change={this.handleInputChange} />
+        <EmployeeList employees={this.state.directory} />
 
       </>
     );
