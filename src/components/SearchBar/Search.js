26e34f851ca755bd48c4cfebf348d9bdcd78d642
Nardin Lachowski <nardin@live.com>
2021-03-27 08:05:47 -0400

    added button for sorting - need to connect to sort.js

--- src/components/SearchBar/Search.js
+++ src/components/SearchBar/Search.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React from "react";
 
 function Search(props) {
-    return (
-        <div className="container">
-        <form>
-          <div className="input-field">
-            <input id="search" type="search" placeholder="Search by name" onChange={props.change} />
-            <label className="label-icon" for="search">
-            </label>
-            <i className="material-icons">close</i>
+  return (
+    <div className="container">
+      <form>
+        <div className="row">
+          <div className="col s8">
+            <div className="input-field">
+              <input
+                id="search"
+                type="search"
+                placeholder="Search by name"
+                onChange={props.change}
+              />
+              <label className="label-icon" for="search"></label>
+              <i className="material-icons">close</i>
+            </div>
           </div>
-        </form>
-      </div>
-    );
+          <div className="col s3">
+            <button className="waves-effect waves-light btn-small">
+              Sort by Experience
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Search;
