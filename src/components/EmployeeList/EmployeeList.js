814901311c321e06e530d691fbeedffb4410361e
Nardin Lachowski <nardin@live.com>
2021-03-26 23:58:51 -0400

    added employeecard components to display on page

--- src/components/EmployeeList/EmployeeList.js
+++ src/components/EmployeeList/EmployeeList.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import EmployeeCard from '../EmployeeCard/EmployeeCard';
 
-function EmployeeList() {
+function EmployeeList(props) {
     return (
-        <>
-        
-        </>
+        <div className="container row">
+            {props.employees.map((person, index) => (
+                <EmployeeCard data={ person } key={ index } />
+            ))}
+        </div>
     );
 }
 
