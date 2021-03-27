51e8fd46587fd1cbc3d492806976f9918f0e0333
Nardin Lachowski <nardin@live.com>
2021-03-26 23:57:50 -0400

    removed some of the original card template content aand passed promps to display data on cards

--- src/components/EmployeeCard/EmployeeCard.js
+++ src/components/EmployeeCard/EmployeeCard.js
@@ -1,45 +1,25 @@
 import React from 'react';
 
-function EmployeeCard() {
+function EmployeeCard(props) {
     return (
-        <>
-        <div className="card">
+        <div className="card col s12 m6 l4">
             <div className="card-image waves-effect waves-block waves-light">
-                <img className="activator" src="images/office.jpg" />
+                <img className="activator" src={props.data.picture.large} />
             </div>
             <div className="card-content">
                 <span className="card-title activator grey-text text-darken-4">
-                    Employee Name <i className="material-icons right">more_vert</i>
+                    {props.data.name.first} {props.data.name.last}<i className="material-icons right">more_vert</i>
                 </span>
-                <p>
-                    <a href="#">This is a link</a>
-                </p>
             </div>
             <div className="card-reveal">
                 <span className="card-title grey-text text-darken-4">
-                    Employee Name<i className="material-icons right">close</i>
+                    {props.data.name.first} {props.data.name.last} ({props.data.dob.age})<i className="material-icons right">close</i><br/><br/>
+                    <p>Phone: {props.data.phone}</p>
+                    <p>Email: {props.data.email}</p>
                 </span>
-                <div className="card-reveal">
-                <span className="card-title grey-text text-darken-4">
-                    Employee Name<i className="material-icons right">close</i>
-                </span>
-                <div className="content">
-                    <ul>
-                        <li>
-                             <strong>Name:</strong> {/*{props.name} */}
-                        </li>
-                        <li>
-                            <strong>Occupation:</strong> {/*{props.occupation}*/}
-                        </li>
-                        <li>
-                            <strong>Address:</strong> {/*{props.location}*/}
-                        </li>
-                    </ul>
-                </div>
-            </div>
+
             </div>
         </div>
-        </>
     );
 }
 
