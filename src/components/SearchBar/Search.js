9c4a23e0123dfc148ba5ebfeb15716fba2aeea9c
Nardin Lachowski <nardin@live.com>
2021-03-27 00:05:06 -0400

    removed search icon, passing props and onchange function

--- src/components/SearchBar/Search.js
+++ src/components/SearchBar/Search.js
@@ -1,13 +1,12 @@
 import React from 'react';
 
-function Search() {
+function Search(props) {
     return (
         <div className="container">
         <form>
           <div className="input-field">
-            <input id="search" type="search" placeholder="Search by name" />
+            <input id="search" type="search" placeholder="Search by name" onChange={props.change} />
             <label className="label-icon" for="search">
-                <i className="material-icons">search</i>
             </label>
             <i className="material-icons">close</i>
           </div>
